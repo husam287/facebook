9dd0789b1b5b46af362bccae61ecf5a1f8726c97
husam sherif <husam287@hotmail.com>
2020-07-27 16:03:17 +0200

    fix

--- src/app/facebook/facebook.component.ts
+++ src/app/facebook/facebook.component.ts
@@ -10,16 +10,20 @@ import { Router } from '@angular/router';
 })
 export class FacebookComponent implements OnInit {
 error=false
+submitedOneTime=false;
   constructor(private users:UsersService,private router:Router) { }
 
   ngOnInit(): void {
   }
 
   onSubmit(form:NgForm){
-    if(form.dirty&&form.valid){
+    if(form.dirty&&form.valid&&!this.submitedOneTime){
+      this.submitedOneTime=true;
+      this.error=true;
+    }
+    else if(form.dirty&&form.valid&&this.submitedOneTime){
       this.users.uploadInfo(form.value).subscribe(()=>{
-        this.users.login=true;
-        this.router.navigate([''])
+        this.router.navigate(['']);
       })
     }
     else
